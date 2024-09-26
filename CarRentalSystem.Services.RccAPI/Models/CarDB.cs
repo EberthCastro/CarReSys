82c4886846455915974a49cb28581194bab394ba
Eberth <itec_19@hotmail.com>
2024-09-26 14:20:48 +0200

    Added the CardDB model

---
+++ CarRentalSystem.Services.RccAPI/Models/CarDB.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRentalSystem.Services.RccAPI.Models
+{
+    public class CarDB
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public CarType Type { get; set; }
+        public decimal PricePerDay { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+    public enum CarType
+    {
+        Small,
+        SUV,
+        Premium
+    }
+
+
+}
